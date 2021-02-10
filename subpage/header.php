491a53f8e1a28aa0fb43beee720b8050f3b35ee1
thien <thiendt1608@gmail.com>
2021-02-10 15:30:27 +0700

    dich sang tieng viet

--- subpage/header.php
+++ subpage/header.php
@@ -14,24 +14,25 @@
                                 <div class="main-menu  d-none d-lg-block">
                                     <nav>
                                         <ul id="navigation">
-                                            <li><a class="active" href="index.php">home</a></li>
-                                            <li><a href="about.php">About</a></li>
-                                            <li><a class="" href="travel_destination.php">Destination</a></l/li>
+                                            <li><a class="active" href="index.php">Trang chủ</a></li>
+                                            <li><a href="about.php">Giới thiệu</a></li>
+                                            <li><a class="" href="travel_destination.php">Tour Du Lịch</a></l/li>
                                             <!-- <li><a href="#">pages <i class="ti-angle-down"></i></a>
                                                 <ul class="submenu">
                                                         <li><a href="destination_details.php">Destinations details</a></li>
                                                         <li><a href="elements.php">elements</a></li>
                                                 </ul>
                                             </li> -->
-                                            <li><a href="destination_details.php">Destinations details</a></li>
+                                            <!-- phần Destinations details sẽ làm chi tiết cho từng tour -->
+                                            <!-- <li><a href="destination_details.php">Destinations details</a></li> -->
                                             <!-- <li><a href="#">blog <i class="ti-angle-down"></i></a>
                                                 <ul class="submenu">
                                                     <li><a href="blog.php">blog</a></li>
                                                     <li><a href="single-blog.php">single-blog</a></li>
                                                 </ul>
                                             </li> -->
-                                            <li><a href="blog.php">Blog</a></li>
-                                            <li><a href="contact.php">Contact</a></li>
+                                            <li><a href="blog.php">Cẩm nang du lịch</a></li>
+                                            <li><a href="contact.php">Liên hệ</a></li>
                                         </ul>
                                     </nav>
                                 </div>
@@ -39,23 +40,23 @@
                             <div class="col-xl-4 col-lg-4 d-none d-lg-block">
                                 <div class="social_wrap d-flex align-items-center justify-content-end">
                                     <div class="number">
-                                        <p> <i class="fa fa-phone"></i> (949)-666 3839</p>
+                                        <p> <i class="fa fa-phone"></i>+84 986719762</p>
                                     </div>
                                     <div class="social_links d-none d-xl-block">
                                         <ul>
                                             <li><a href="#"> <i class="fa fa-instagram"></i> </a></li>
-                                            <li><a href="#"> <i class="fa fa-linkedin"></i> </a></li>
-                                            <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
-                                            <li><a href="#"> <i class="fa fa-google-plus"></i> </a></li>
+                                            <!-- <li><a href="#"> <i class="fa fa-linkedin"></i> </a></li> -->
+                                            <li><a href="https://www.facebook.com/thien.nho.140/"> <i class="fa fa-facebook"></i> </a></li>
+                                            <!-- <li><a href="#"> <i class="fa fa-google-plus"></i> </a></li> -->
                                         </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="seach_icon">
+                            <!-- <div class="seach_icon">
                                 <a data-toggle="modal" data-target="#exampleModalCenter" href="#">
                                     <i class="fa fa-search"></i>
                                 </a>
-                            </div>
+                            </div> -->
                             <div class="col-12">
                                 <div class="mobile_menu d-block d-lg-none"></div>
                             </div>
